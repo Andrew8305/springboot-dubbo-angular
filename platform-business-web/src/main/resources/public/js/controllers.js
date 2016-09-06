f9e1fc7be9ae0de006d24f0745d130fed0cc60f0
zhangxd1989 <zhangxd_2011sz@163.com>
2016-09-06 10:20:09 +0800

    update package name

---
+++ platform-business-web/src/main/resources/public/js/controllers.js
@@ -0,0 +1,44 @@
+/**
+ * INSPINIA - Responsive Admin Theme
+ *
+ */
+
+/**
+ * MainCtrl - controller
+ */
+function MainCtrl($scope, $state) {
+
+    this.userName = 'Example user';
+    this.helloText = 'Welcome in SeedProject';
+    this.descriptionText = 'It is an application skeleton for a typical AngularJS web app. You can use it to quickly bootstrap your angular webapp projects and dev environment for these projects.';
+
+    $scope.$on('$stateChangeStart', function(scope, next, current) {
+        debugger
+        $state.go('login');
+    });
+}
+
+function LoginCtrl($scope, $http, $state) {
+    $scope.user = {};
+    $scope.authError = null;
+    $scope.login = function() {
+        $scope.authError = null;
+        // Try to login
+        $http.post('/auth', {username: $scope.user.username, password: $scope.user.password})
+            .then(function(response) {
+                if ( !response.data.token ) {
+                    $scope.authError = 'Email or Password not right';
+                }else{
+                    $state.go('index.main');
+                }
+            }, function(x) {
+                $scope.authError = 'Server Error';
+            });
+    };
+}
+
+angular
+    .module('inspinia')
+    .controller('MainCtrl', ['$scope', '$state', MainCtrl])
+    .controller('LoginCtrl', ['$scope', '$http', '$state', LoginCtrl])
+;
